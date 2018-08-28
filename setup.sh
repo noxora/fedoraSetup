02f72ed77554ee3b9eb9e0c412f8969c7029a13a
Liam <liam@localhost.localdomain>
2018-08-28 12:07:35 -0400

    added more stuff

--- setup.sh
+++ setup.sh
@@ -1,35 +1,38 @@
 #!/bin/bash
 
-dnf update
+sudo dnf update
 
 #Install VLC for codecs
-dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
-dnf install vlc
+sudo dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf install vlc
 echo "\n\nCODECS HAVE BEEN INSTALLED\n\n"
 
 #Install gnome tweak
-dnf install gnome-tweak-tool
+sudo dnf install gnome-tweak-tool
 echo "\n\nGNOME TWEAK TOOL HAS BEEN INSTALLED\n\n"
 
 #Install Spotify
-dnf config-manager --add-repo=https://negativo17.org/repos/fedora-spotify.repo
-dnf -y install spotify-client
+sudo dnf config-manager --add-repo=https://negativo17.org/repos/fedora-spotify.repo
+sudo dnf -y install spotify-client
 echo "\n\nSPOTIFY HAS BEEN INSTALLED\n\n"
 
 #Install Steam
-dnf config-manager --add-repo=https://negativo17.org/repos/fedora-steam.repo
-dnf -y install steam
+sudo dnf config-manager --add-repo=https://negativo17.org/repos/fedora-steam.repo
+sudo dnf -y install steam
 echo "\n\nSTEAM HAS BEEN INSTALLED\n\n"
 
 #Install VSCODE
-rpm --import https://packages.microsoft.com/keys/microsoft.asc
-sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
-dnf check-update
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+sudo dnf check-update
 sudo dnf install code
+echo "\n\nCODE HAS BEEN INSTALLED\n\n"
 
 #Install ZSH
-dnf install zsh
+sudo dnf install zsh
 chsh -s $(which zsh)
+mv .zshrc ~/.zshrc
+echo "\n\nZSH HAS BEEN INSTALLED\n\n"
 
 #Install pycharm
 $PYCHARM="pycharm-community-2018.2.2"
